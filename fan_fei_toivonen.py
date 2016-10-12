b3813235fb9799e6515b774bc2ce0929d9946ac6
fanf <fanf@oupeng.com>
2016-10-11 23:32:48 -0700

    q2

--- fan_fei_toivonen.py
+++ fan_fei_toivonen.py
@@ -2,7 +2,7 @@ import sys
 #from functools import reduce
 import itertools
 import json
-
+import random
 
 
 def init_dict(dict, bucket_size):
@@ -11,37 +11,50 @@ def init_dict(dict, bucket_size):
         dict[i] = 0
         i += 1
 
+def hash_func_1(x):
+    return ord(x.strip()) + 1
 
-def elem_std(elem):
-    return elem.strip()
+def hash_func_2(x):
+    return ord(x.strip()) * 9
 
-def is_validate(elem, res, num):
+
+def is_validate(elem, freq_set, num):
     if num == 1:
         return True
     for sub_elem in itertools.combinations(elem, num - 1):
-        if sub_elem not in res[num - 1]:
+        if sub_elem not in freq_set:
             return False
     return True
 
 def freq_set_to_str(freq_set):
     return "[" + ", ".join(map(str, map(list, sorted(list(freq_set))))) + "]"
 
+def elem_std(elem):
+    return elem.strip()
 
-def find_freq_item_set():
-    pass
+def input_to_std(lines):
+    std_lines = []
+    for line in lines:
+        line = sorted(map(elem_std, line.split(",")))
+        std_lines.append(line)
+    return std_lines
 
+def sample_lines(lines, rate):
+    size = len(lines)
+    sample_index = random.sample(range(size), int(rate * size))
+    sampled_std_lines = []
+    for index in sample_index:
+        sampled_std_lines.append(lines[index])
+    return  sampled_std_lines
 
-
-def output(dict_1, dict_2, freq_set):
-    with open("output_multihash.txt", "a") as file:
-        json.dump(dict_1, file)
-        file.write("\n")
-        json.dump(dict_2, file)
-        file.write("\n")
-        file.write(freq_set_to_str(freq_set))
-        file.write("\n\n")
+def output(time, fraction, res):
+    with open("output_toivonen.txt", "a") as file:
+        file.write(str(time) + "\n")
+        file.write(str(fraction) + "\n")
+        for freq_set in res:
+            file.write(freq_set_to_str(freq_set))
+            file.write("\n\n")
         #print(sorted(list(freq_set)))
-
 
 
 def _main():
@@ -51,12 +64,126 @@ def _main():
 
     filename = "input.txt"
     support = 4
-    bucket_size = 5
+
+    fraction = 0.5
+    lower_rate = 0.35
+    max_try = 100
+    does_finished = False
 
     with open(filename, 'r') as input:
         lines = input.readlines()
+    std_lines = input_to_std(lines)
+
+    res = []
+
+    time = 1
+    while time < max_try:
+
+        sampled_std_lines = sample_lines(std_lines, fraction)
+        bucket_size = len(sampled_std_lines) * 5
+
+        print(sampled_std_lines)
+
+        former_freq = {}
+        res = []
+        is_fail = False
+
+        num = 1
+        while True:
+            dict_1 = {}
+            dict_2 = {}
+
+            init_dict(dict_1, bucket_size)
+            init_dict(dict_2, bucket_size)
+
+            negative_border = {}
+
+            for line in sampled_std_lines:
+                for elem in itertools.combinations(line, num):
+                    print(elem)
+                    if not is_validate(elem, former_freq, num):
+                        continue
+                    key_1 = sum(map(hash_func_1, elem)) % bucket_size
+                    dict_1[key_1] += 1
+                    key_2 = sum(map(hash_func_2, elem)) % bucket_size
+                    dict_2[key_2] += 1
+                    print(dict_1)
+                    print(dict_2)
+
+            freq_map = {}
+
+            for line in sampled_std_lines:
+                for elem in itertools.combinations(line, num):
+                    print(elem)
+                    print(freq_map)
+                    if not is_validate(elem, former_freq, num):
+                        continue
+                    if dict_1[sum(map(hash_func_1, elem)) % bucket_size] < support*lower_rate or dict_2[
+                                sum(map(hash_func_2, elem)) % bucket_size] < support*lower_rate:
+                        negative_border[elem] = 0
+                        print("add neg")
+                        continue
+                    if elem in freq_map:
+                        freq_map[elem] += 1
+                    else:
+                        freq_map[elem] = 1
+
+            print(freq_map)
+            former_freq = {}
+            for key, value in freq_map.items():
+                print(key)
+                print(value)
+                if value >= support*lower_rate:
+                    former_freq[key] = 0
+                else:
+                    negative_border[key] = 0
+
+            print(negative_border)
+
+            for line in std_lines:
+                for elem in itertools.combinations(line, num):
+                    if elem in negative_border:
+                        negative_border[elem] += 1
+                    elif elem in former_freq:
+                        former_freq[elem] += 1
+
+            for key, value in negative_border.items():
+                if value >= support:
+                    is_fail = True
+
+            if is_fail == True:
+                break
+
+            cur_res = set()
+            print("FFFFFFF")
+            print(former_freq)
+            for key, value in former_freq.items():
+                if value >= support:
+                    cur_res.add(key)
+
+            if len(cur_res) == 0:
+                does_finished = True
+                print("!!!!!!!!!!!!!!!!!")
+                break
+
+            res.append(cur_res)
+            print("RESSSSSSS")
+            print(res)
+
+            num += 1
+
+            print("###############")
+            print("###############")
+            print("###############")
+        print("&&&&&&&")
+        if does_finished == True:
+            break
+        time += 1
 
 
+
+    if does_finished == True:
+        output(time , fraction, res)
 
 
 _main()
